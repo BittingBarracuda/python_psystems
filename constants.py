81687a9ac8375d8869d074ce0776779e0e0c5287
BittingBarracuda <kmartinez2397@gmail.com>
2024-06-22 23:37:52 +0200

    Small fixes.

--- constants.py
+++ constants.py
@@ -1,8 +1,4 @@
 DEST_HERE = 'here'
 DEST_OUT = 'out'
-DEST_IN = 'in-'
 
-DESTS = [DEST_HERE, DEST_OUT, DEST_IN]
-
-def check_correct_dest(x):
-    return (x in DESTS[:2]) or (x.startswith(DESTS[-1]))
+DESTS = [DEST_HERE, DEST_OUT]
