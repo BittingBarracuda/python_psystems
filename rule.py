0ab0063b2c9c32af299efa2fa68327189a88bd90
BittingBarracuda <kmartinez2397@gmail.com>
2024-06-22 23:37:41 +0200

    Implementation of rule priority.

--- rule.py
+++ rule.py
@@ -1,15 +1,13 @@
 from multiset import Multiset
-from constants import check_correct_dest, DESTS
+from constants import DESTS
 
 class Rule():
     def __init__(self, lhs, rhs, dest='here', priority=1.0):
         if (type(lhs) != Multiset) or (type(rhs) != Multiset):
             raise TypeError('Both left-hand side and right-hand side should be an instance of Multiset!')
         if (type(priority != float)):
-            raise TypeError()
+            raise TypeError('Priority should be defined as a decimal number!')
         self.lhs = lhs 
         self.rhs = rhs
-        if check_correct_dest(dest):
-            self.destination = dest
-        else:
-            raise ValueError(f'Destination should have one of the following formats:\n{DESTS}')
+        self.destination = dest
+        self.priority = priority
