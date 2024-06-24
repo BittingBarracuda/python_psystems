d7dcc322c5a5cbd1866dcbf34450192f5a90f10b
BittingBarracuda <kmartinez2397@gmail.com>
2024-06-24 18:54:04 +0200

    Implemented functionality to read alphabet from rules file.

--- tests.py
+++ tests.py
@@ -1,8 +1,12 @@
 from xml_parser import read_config, read_rules
+from datetime import datetime
 from multiset import Multiset
 from membrane import Membrane
 from rule import Rule
 import time
+
+def get_datetime():
+    return datetime.now().strftime("%d/%m/%Y %H:%M:%S")
 
 if __name__ == "__main__":
     # rules_1 = [Rule(Multiset('a'), Multiset('b'), 'here', 1.0),
@@ -13,14 +17,20 @@ if __name__ == "__main__":
     # membrane_2.set_parent(membrane_1)
 
     start_time = time.time()
-    print(f'[!] Starting computation...')
-    print(f'[!] Reading config file...')
+    print(f'[! {get_datetime()}] Starting computation...')
+    
+    print(f'[! {get_datetime()}] Reading config file...')
     membrane_1 = read_config()
-    print(f'[!] Reading rules file...')
-    rules = read_rules()
+   
+    print(f'[! {get_datetime()}] Reading rules file...')
+    rules, alphabet = read_rules()
+    
     membrane_1.set_rules(rules)
-    print(f'[!] Computing all steps...')
-    # membrane_1.run(num_steps=100_000)
-    print(f'[!] Ending computation...')
+    all_membranes = membrane_1.get_all_membranes()
+    print(f'[! {get_datetime()}] System contains {len(all_membranes)} membranes!')
+    
+    print(f'[! {get_datetime()}] Computing all steps...')
+    membrane_1.run(num_steps=1_00)
+    print(f'[! {get_datetime()}] Ending computation...')
     end_time = time.time()
-    print(f'[!] Time elapsed: {end_time - start_time} seconds!')
+    print(f'[! {get_datetime()}] Time elapsed: {end_time - start_time} seconds!')
