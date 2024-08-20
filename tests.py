db5cdf7337268a239e49cf3d34668ef1b439caf8
BittingBarracuda <kmartinez2397@gmail.com>
2024-08-20 20:03:46 +0200

    Implemented option to run the program with cProfile that creates a readable output txt file.

--- tests.py
+++ tests.py
@@ -6,6 +6,9 @@ from rule import Rule
 from tqdm import tqdm
 import time
 
+import cProfile
+import pstats
+
 def get_datetime():
     return datetime.now().strftime("%d/%m/%Y %H:%M:%S")
 
@@ -24,6 +27,17 @@ def membranes_to_txt(membranes):
                 parent_id = ''
             file.write(f'membrane_id={membrane.id},parent_id={parent_id},contents=[{contents}], rules=[{rules_str}]\n')
 
+def run_with_cprofile(membrane_1):
+    prof = cProfile.Profile()
+    prof.run('membrane_1.run(num_steps=1_00)')
+    # prof.sort_stats('cumtime')
+    prof.dump_stats('output.prof')
+
+    stream = open('output_1.txt', 'w')
+    stats = pstats.Stats('output.prof', stream=stream)
+    stats.sort_stats('cumtime')
+    stats.print_stats()
+
 if __name__ == "__main__":
     print(f'[{get_datetime()}] Starting computation...')
     print(f'[{get_datetime()}] Reading rules file...')
@@ -36,10 +50,11 @@ if __name__ == "__main__":
     all_membranes = membrane_1.get_all_membranes()
     print(f'[{get_datetime()}] System contains {len(all_membranes)} membranes!')
     
-    print(f'[{get_datetime()}] Writing membranes to txt file...')
+    # print(f'[{get_datetime()}] Writing membranes to txt file...')
     start_time = time.time()
     print(f'[{get_datetime()}] Computing all steps...')
     membrane_1.run(num_steps=1_00)
+    # run_with_cprofile(membrane_1)
     print(f'[{get_datetime()}] Ending computation...')
     end_time = time.time()
     print(f'[{get_datetime()}] Time elapsed: {end_time - start_time} seconds!')
